c1c0892287049701ef63aeca8a8f3068faf02cf0
mehdi <mhd.mosavi@gmail.com>
2016-06-21 18:00:53 +0430

    add specification value in create test data

--- products/bulk_insert.py
+++ products/bulk_insert.py
@@ -1,4 +1,4 @@
-from .models import Category, Product, Condition
+from .models import Category, Product, Condition, Specification, SpecificationValue
 from datetime import datetime
 
 def bulk_category():
@@ -23,8 +23,6 @@ def bulk_product_tablet():
                 Product(name='Galaxy tab s1',created_at=datetime.now(),is_container = True,category=category),
                 Product(name='Galaxy tab s3',created_at=datetime.now(),is_container = True,category=category),
                 Product(name='Galaxy note1',created_at=datetime.now(),is_container = True,category=category),
-                Product(name='Galaxy note1',created_at=datetime.now(),is_container = True,category=category),
-                Product(name='Galaxy note1',created_at=datetime.now(),is_container = True,category=category),
                 Product(name='Galaxy note2',created_at=datetime.now(),is_container = True,category=category),
                 Product(name='Galaxy note3',created_at=datetime.now(),is_container = True,category=category),
                 Product(name='Galaxy note4',created_at=datetime.now(),is_container = True,category=category),
@@ -43,11 +41,59 @@ def bulk_condition():
         ])
     print("bulk Condition inserted!!!")
 
+def bulk_specification():
+    Specification.objects.bulk_create(
+        [
+            Specification(created_time=datetime.now(), title='برند', slug='brand'),
+            Specification(created_time=datetime.now(), title='رم', slug='ram'),
+            Specification(created_time=datetime.now(), title='پردازشگر مرکزی', slug='cpu'),
+            Specification(created_time=datetime.now(), title='حافظه داخلی', slug='hard drive'),
+            Specification(created_time=datetime.now(), title='صفحه نمایش', slug='display'),
+        ]
+    )
+    print("bulk Specification inserted!!!")
+
+def bulk_specification_value_brand():
+    spec = Specification.objects.get(pk=1)
+    SpecificationValue.objects.bulk_create(
+        [
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='اپل', slug='apple'),
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='سامسونگ', slug='samsung'),
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='سونی', slug='sony'),
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='هوواویی', slug='hauweii'),
+        ]
+    )
+    print("bulk bulk_specification_value_brand inserted!!!")
+
+
+def bulk_specification_value_ram():
+    spec = Specification.objects.get(pk=2)
+    SpecificationValue.objects.bulk_create(
+        [
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='۲ گیگابایت', slug='2 GB'),
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='۳ گیگابایت', slug='3 GB'),
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='۴ گیگابایت', slug='4 GB'),
+            SpecificationValue(created_time=datetime.now(), specification=spec, display_value='۸ گیگابایت', slug='8 GB'),
+        ]
+    )
+    print("bulk bulk_specification_value_ram inserted!!!")
+
+def bulk_spec_val():
+    bulk_specification_value_brand()
+    bulk_specification_value_ram()
 
 def bulk_test_data():
     bulk_category()
     bulk_product_tablet()
     bulk_condition()
+    bulk_specification()
+    bulk_spec_val()
+
+
+
+
+
+
 
 
 
