990be89218dd83691d519fe5a2228363b3b8af1b
mehdi <mhd.mosavi@gmail.com>
2016-07-20 15:53:14 +0430

    add filter in setting

--- E_Shop/settings.py
+++ E_Shop/settings.py
@@ -105,3 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+        'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
+        }
