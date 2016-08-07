f8c78ba35e6cb10a9d9873c87c5c441e7e26bad2
mehdi <mhd.mosavi@gmail.com>
2016-08-07 18:01:09 +0430

    add category to template

--- location/views.py
+++ location/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from .models import Country
 # Create your views here.
 
 class GreetingView(View):
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Category
 from django.views.generic import View
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
@@ -13,6 +13,18 @@ def index(request):
             'product_list': product_list,
     }
     return HttpResponse(template.render(context, request))
+
+def category_list(request):
+    category_list = Category.objects.all()
+    template = loader.get_template('products/category.html')
+    context = {
+            'category_list': category_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def category_detail(request, category_id):
+        category = get_object_or_404(Category, pk = category_id)
+        return  render(request,'products/category_detail.html', {'category': category}) 
 
 def current_datetime(request):
         now = datetime.datetime.now()
